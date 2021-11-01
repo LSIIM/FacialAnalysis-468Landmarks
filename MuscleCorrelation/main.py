1c5fe5b99a4c03e87aa0b162288c2b2b047c27c2
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-11-01 09:29:58 -0300

    deixei mais facil para analisar os pontos e adicionar novos músculos

--- MuscleCorrelation/main.py
+++ MuscleCorrelation/main.py
@@ -1,33 +1,48 @@
 from FaceMeshModule import FaceMeshDetector
 import cv2
-import muscles_identification as muscId
+import pandas as pd
+import numpy as np
+from muscles_identification import *
+
+
 def detect_face_on_image(path):
-    detector = FaceMeshDetector(maxNumFaces=1,minDetectionConfidence=0.9)
+    detector = FaceMeshDetector(maxNumFaces=1, minDetectionConfidence=0.8)
     name = path.split('/')
     name = name[len(name)-1].split('.')
     name = name[0]
     img = cv2.imread(path)
-    img2,faces = detector.findFaceMesh(img)
+    img_f, faces = detector.findFaceMesh(img)
     print(name)
-    for face in faces:
-        for i,lms in enumerate(face):
-            achou = False
-            for muscle in muscId.muscle_list:
-                if(i in muscle.muscle_points):
-                    achou = True
-                    cv2.putText(img2,".",lms,cv2.FONT_HERSHEY_PLAIN,6,(0,0,0),4) 
-                    cv2.putText(img2,str(i),lms,cv2.FONT_HERSHEY_PLAIN,1.4,muscle.color,1)                        
-                    break
-            if(not achou):
-                cv2.putText(img2,str(i),lms,cv2.FONT_HERSHEY_PLAIN,1.1,(0,255,0),1)
+    #cv2.namedWindow('Muscles', cv2.WINDOW_NORMAL)
+
+    while(1):
+        img2 = img_f.copy()
+        muscles = get_muscles_from_csv()
+        for face in faces:
+            for i, lms in enumerate(face):
+                achou = False
+                for muscle in muscles:
+                    if(i in muscle.muscle_points):
+                        achou = True
+                        cv2.putText(img2, ".", lms,
+                                    cv2.FONT_HERSHEY_PLAIN, 8, (0, 0, 0), 4)
+                        cv2.putText(img2, str(i), lms,
+                                    cv2.FONT_HERSHEY_PLAIN, 2, muscle.color, 1)
+                        break
+                if(not achou):
+                    cv2.putText(img2, str(i), lms,
+                                cv2.FONT_HERSHEY_PLAIN, 1.6, (0, 255, 0), 1)
+
+        cv2.putText(img2, "Legenda de cores", (100, 300),
+                    cv2.FONT_HERSHEY_PLAIN, 10, (0, 0, 0), 3)
+        for i, muscle in enumerate(muscles):
+            cv2.putText(img2, muscle.muscle_name, (100, 500 + 200*i),
+                        cv2.FONT_HERSHEY_PLAIN, 10, muscle.color, 3)
+        cv2.imshow("Muscles", cv2.resize(
+            img2, (1000, 1000), interpolation=cv2.INTER_AREA))
+        cv2.waitKey(1)
+        cv2.imwrite(name+"-landmarks-colored.jpg", img2)
 
 
-    cv2.putText(img2,"Legenda de cores",(100,300),cv2.FONT_HERSHEY_PLAIN,10,(0,0,0),3)           
-    for i,muscle in enumerate(muscId.muscle_list):
-        print(i)
-        print(muscle)
-        cv2.putText(img2,muscle.muscle_name,(100,500 + 200*i),cv2.FONT_HERSHEY_PLAIN,10,muscle.color,3) 
-    cv2.imwrite(name+"-landmarks-colored.jpg",img2)
-     
 if __name__ == "__main__":
     detect_face_on_image("./human-muscles.jpg")
--- MuscleCorrelation/muscles_identification.py
+++ MuscleCorrelation/muscles_identification.py
@@ -1,8 +1,12 @@
+import pandas as pd
+
+
 class Muscle:
-    def __init__(self,muscle_name,muscle_point_list,color):
+    def __init__(self, muscle_name, muscle_point_list, color):
         self.muscle_name = muscle_name
         self.muscle_points = muscle_point_list
         self.color = color
+
     def __str__(self):
         points_str = "["
         for point in self.muscle_points:
@@ -11,8 +15,59 @@ class Muscle:
                 points_str = points_str + ","
             else:
                 points_str = points_str + "]"
-        return str(self.muscle_name) + " ("+ str(self.color[0]) + ","+ str(self.color[1]) + ","+ str(self.color[2])+ "): " + points_str
+        return str(self.muscle_name) + " (" + str(self.color[0]) + "," + str(self.color[1]) + "," + str(self.color[2]) + "): " + points_str
+
+
 muscle_list = [
-    Muscle("musculo 1",[1,2,3,4,5,6,7,8,9],(255,0,0)),
-     Muscle("musculo 2",[10,11,12,13,14,15],(0,0,255))
+    Muscle("musculo 1", [1, 2, 3, 4, 5, 6, 7, 8, 9], (255, 0, 0)),
+    Muscle("musculo 2", [10, 11, 12, 13, 14, 15], (0, 0, 255)),
+    Muscle("musculo 3", [25, 90], (69, 0, 255)),
+    Muscle("GHJK 4", [60, 42], (11, 99, 11)),
+    Muscle("musc 5", [70, 142, 420], (0, 99, 110)),
+    Muscle("musc 9", [99, 31], (100, 99, 110))
 ]
+
+
+def get_muscles_from_csv():
+    df = pd.read_csv("./muscles.csv")
+    ms_list = []
+    for i in range(0, len(df)):
+        pts = df["points"][i].split(",")
+        pts_int = []
+        for j in range(len(pts)):
+            if(pts[j] != ''):
+                pts_int.append(int(pts[j]))
+
+        col = df["color"][i].split(',')
+        ms_list.append(
+            Muscle(
+                df["name"][i],
+                pts_int,
+                (int(col[0].split("(")[1]), int(
+                    col[1]), int(col[2].split(")")[0]))
+            )
+        )
+    return ms_list
+
+
+def save_csv():
+    names = []
+    points = []
+    colors = []
+    for muscle in muscle_list:
+        names.append(muscle.muscle_name)
+        pts = ""
+        for pt in muscle.muscle_points:
+            pts = pts + str(pt) + ","
+        points.append(pts)
+        colors.append(muscle.color)
+    df = pd.DataFrame()
+    df["name"] = names
+    df["points"] = points
+    df["color"] = colors
+
+    df.to_csv("muscles.csv")
+
+
+if __name__ == "__main__":
+    save_csv()
