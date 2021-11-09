94af1cba03016b4f51cbb7841687bc134e08027d
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-11-09 12:10:34 -0300

    melhorei os escopos das funções, mas ainda tem um erro de 2px no posicionamento e redetectar é burro, mas vou arrumar isso depois

--- 01-face_points_exp/analyser.py
+++ 01-face_points_exp/analyser.py
@@ -2,30 +2,12 @@ from face_adjustments import FaceAdjuster
 from face_mash import FaceMashDetector
 import os
 import cv2
+import numpy as np
 
 DATASET_PATH = r"D:\OneDrive - Etec Centro Paula Souza\Academico\UFSC\MIGMA\migma_dataset"
 
 
-def image_resize(image, width=None, height=None, inter=cv2.INTER_AREA):
-    dim = None
-    (h, w) = image.shape[:2]
-
-    if width is None and height is None:
-        return image
-
-    if width is None:
-        r = height / float(h)
-        dim = (int(w * r), height)
-
-    else:
-        r = width / float(w)
-        dim = (width, int(h * r))
-
-    resized = cv2.resize(image, dim, interpolation=inter)
-    return resized
-
-
-def analyseFace(image, name):
+def fixFacePosition(image):
     meshed_orig = FaceMashDetector(image=image)
     if(not meshed_orig.findFaceMesh()):
         return False
@@ -34,33 +16,67 @@ def analyseFace(image, name):
 
     adjuster = FaceAdjuster(image, lms_orig[0])
     eyes_cent_img = adjuster.alignEyes()
-    eyes_cent_img = image_resize(eyes_cent_img, height=550)
-    #cv2.imshow("orig", image)
-    cv2.imshow("Olhos", eyes_cent_img)
-    cv2.waitKey(0)
-    meshed_crop = FaceMashDetector(image=eyes_cent_img)
+
+    meshed_forehead = FaceMashDetector(image=eyes_cent_img)
+    if(not meshed_forehead.findFaceMesh()):
+        return False
+
+    lms_forehead = meshed_forehead.getLms()
+    if(lms_forehead == []):
+        print("Erro, nenhum rosto detectado")
+        return None
+    else:
+        lms_forehead = lms_forehead[0]
+    adjuster = FaceAdjuster(eyes_cent_img, lms_forehead)
+    eyes_forehead_fix = adjuster.alignFace()
+
+    meshed_crop = FaceMashDetector(image=eyes_forehead_fix)
     if(not meshed_crop.findFaceMesh()):
         return False
 
-    drawImg = eyes_cent_img.copy()
     lms_crop = meshed_crop.getLms()
-    for lm in lms_crop[0]:
-        cv2.putText(drawImg, ".", lm, cv2.FONT_HERSHEY_PLAIN,
-                    0.8, (0, 255, 0), 1)
-    cv2.imshow("new_face", drawImg)
+    if(lms_crop == []):
+        print("Erro, nenhum rosto detectado")
+        return None
+    else:
+        lms_crop = lms_crop[0]
+    adjuster = FaceAdjuster(eyes_forehead_fix, lms_crop)
+    crop_image = adjuster.faceCrop()
+
+    meshed_final = FaceMashDetector(image=crop_image)
+    if(not meshed_final.findFaceMesh()):
+        return False
+
+    lms_final = meshed_final.getLms()
+    if(lms_final == []):
+        print("Erro, nenhum rosto detectado")
+        return None
+    else:
+        lms_final = lms_final[0]
+    print(lms_final[10])
+
+    # -------- Teste -----------------
+    #cv2.imshow("orig", image)
+    cv2.imshow("crop", crop_image)
     cv2.waitKey(0)
+    # ---------------------------------
+    return eyes_cent_img
+
+
+def analyseFace(image, name):
+    fixed_image = fixFacePosition(image)
 
     return True
 
 
 if __name__ == "__main__":
     print("Começando analise")
-    test_path = r"D:\OneDrive - Etec Centro Paula Souza\Academico\UFSC\MIGMA\migma_dataset\00\00\01235"
+    test_path = r"D:\OneDrive - Etec Centro Paula Souza\Academico\UFSC\MIGMA\migma_dataset\00\00\04438"
     fotos = os.listdir(test_path)
-    path = test_path + "/"+fotos[0]
+    path = test_path + "/"+fotos[1]
 
     img = cv2.imread(path)
-    analyseFace(img, fotos[0])
+    analyseFace(img, fotos[1])
     '''
     expressions = os.listdir(DATASET_PATH)
     for exp in expressions:
--- 01-face_points_exp/face_adjustments.py
+++ 01-face_points_exp/face_adjustments.py
@@ -13,9 +13,9 @@ class FaceAdjuster():
 
         xR, yR = self._lms[362]
         xL, yL = self._lms[133]
-
         rotated = ndimage.rotate(self._img, math.atan(
             (yR-yL)/(xR-xL))*180/3.14)
+
         drawImg = self._img.copy()
 
         cv2.putText(drawImg, ".", (xR, yR), cv2.FONT_HERSHEY_PLAIN,
@@ -23,8 +23,22 @@ class FaceAdjuster():
 
         cv2.putText(drawImg, ".", (xL, yL), cv2.FONT_HERSHEY_PLAIN,
                     0.8, (0, 255, 0), 1)
-        #cv2.imshow("img", drawImg)
+        # cv2.imshow("img", drawImg)
 
+        return rotated
+
+    # The 2 paramiters lms are the 2 points of the face that will
+    # be used to center the face and align it
+    def alignFace(self):
+        pos = self._lms[10]
+        cols, rows = self._img.shape[:2]
+        M = np.float32(
+            [[1, 0, int(cols/2)-int(pos[0])], [0, 1, 100-int(pos[1])]])
+        dst = cv2.warpAffine(self._img, M, (cols, rows))
+
+        return dst
+
+    def faceCrop(self):
         top, left, bottom, right = self._find_face_border()
         cent_img = []
         print(top, left, bottom, right)
@@ -42,15 +56,28 @@ class FaceAdjuster():
                 row.append(self._img[j][i])
             cent_img.append(row)
         cent_img = np.array(cent_img)
+        cent_img = self._image_resize(cent_img, height=550)
         return cent_img
-
-    # The 2 paramiters lms are the 2 points of the face that will
-    # be used to center the face and align it
-    def alignFace(self, lmsUp, lmsDown):
-        return True
-
     # private mathods
 # ------------------------------------------------------
+
+    def _image_resize(self, img, width=None, height=None, inter=cv2.INTER_AREA):
+        dim = None
+        (h, w, _) = img.shape
+
+        if width is None and height is None:
+            return img
+
+        if width is None:
+            r = height / float(h)
+            dim = (int(w * r), height)
+
+        else:
+            r = width / float(w)
+            dim = (width, int(h * r))
+
+        resized = cv2.resize(img, dim, interpolation=inter)
+        return resized
 
     def _getImage(self):
         return self._img
