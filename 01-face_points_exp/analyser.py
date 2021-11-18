2ae465e1ff97e7c9e892565cb84f3e9e654c5f5a
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-11-18 20:07:51 -0300

    .

--- 01-face_points_exp/analyser.py
+++ 01-face_points_exp/analyser.py
@@ -1,7 +1,6 @@
 from face_adjustments import FaceAdjuster
 from face_mash import FaceMashDetector
 from definitions import *
-from triangles import TRIANGLES
 
 import os
 import cv2
@@ -47,21 +46,6 @@ def analyseFace(image, extractor):
         return [], adjuster.error
 
     nlms = adjuster.getLms()
-    '''image = adjuster.getImg()
-    for lm in nlms:
-        cv2.putText(image, str("."), lm, cv2.FONT_HERSHEY_PLAIN,
-                    0.8, (0, 255, 0), 1)
-
-    for tria in TRIANGLES:
-        image = cv2.line(image, nlms[tria[0]],
-                         nlms[tria[1]], (0, 0, 255), 1)
-        image = cv2.line(image, nlms[tria[0]],
-                         nlms[tria[2]], (0, 0, 255), 1)
-        image = cv2.line(image, nlms[tria[1]],
-                         nlms[tria[2]], (0, 0, 255), 1)
-
-    cv2.imshow("img", image)
-    cv2.waitKey(0)'''
 
     return nlms, None
 
