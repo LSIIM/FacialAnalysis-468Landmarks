7a80b53c1c817ae63c62e5ac78eef125f4e5cae2
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-11-18 23:10:34 -0300

    adicionei as fotos das mascaras normais e fantasmas

--- 01-face_points_exp/definitions.py
+++ 01-face_points_exp/definitions.py
@@ -4,3 +4,5 @@ face_height = 320
 face_margin = 25
 final_image_size_width = 600
 final_image_size_height = 600
+emotions = ["Neutral", "Angry", "Contempt",
+            "Disgusting", "Fear", "Happy", "Sad", "Surprise"]
--- 01-face_points_exp/draw_triangles.py
+++ 01-face_points_exp/draw_triangles.py
@@ -1,19 +1,60 @@
 
 from triangles import TRIANGLES
+import os
+import numpy as np
+import pandas as pd
+import cv2
 
-
-image = adjuster.getImg()
-for lm in nlms:
-    cv2.putText(image, str("."), lm, cv2.FONT_HERSHEY_PLAIN,
-                0.8, (0, 255, 0), 1)
-
-for tria in TRIANGLES:
-    image = cv2.line(image, nlms[tria[0]],
-                     nlms[tria[1]], (0, 0, 255), 1)
-    image = cv2.line(image, nlms[tria[0]],
-                     nlms[tria[2]], (0, 0, 255), 1)
-    image = cv2.line(image, nlms[tria[1]],
-                     nlms[tria[2]], (0, 0, 255), 1)
-
-cv2.imshow("img", image)
-cv2.waitKey(0)
+expressions = os.listdir("../processed")
+for exp in expressions:
+    mean_exp = np.zeros((600, 600, 3), dtype=float)
+    num = 0
+    if(len(exp.split(".")) > 1):
+        continue
+    types = os.listdir("../processed/"+exp)
+    for tp in types:
+        if(len(tp.split(".")) > 1):
+            continue
+        users = os.listdir("../processed/"+exp+"/"+tp)
+        for user in users:
+            infos = os.listdir("../processed/"+exp+"/"+tp+"/"+user)
+            mean_user = np.zeros((600, 600, 3), dtype=float)
+            for doc in infos:
+                if(doc.split(".")[1] == "csv"):
+                    df = pd.read_csv("../processed/"+exp +
+                                     "/"+tp+"/"+user+"/"+doc)
+                    image = np.zeros((600, 600, 3), dtype=float)
+                    x = df["x"].tolist()
+                    y = df["y"].tolist()
+                    lms = []
+                    for i in range(len(x)):
+                        lms.append([x[i], y[i]])
+                    for tria in TRIANGLES:
+                        image = cv2.line(image, lms[tria[0]],
+                                         lms[tria[1]], (255, 255, 255), 1)
+                        image = cv2.line(image, lms[tria[0]],
+                                         lms[tria[2]], (255, 255, 255), 1)
+                        image = cv2.line(image, lms[tria[1]],
+                                         lms[tria[2]], (255, 255, 255), 1)
+                    cv2.imwrite("../processed/"+exp +
+                                "/"+tp+"/"+user+"/" + doc.split(".")[0]+'.jpg', image)
+                    cv2.imshow("img", image)
+                    mean_user = mean_user + image
+                    mean_exp = mean_exp + image
+                    num += 1
+                    cv2.waitKey(1)
+            mean_user /= 3
+            mean_user *= -1
+            mean_user += 255
+            mean_user = np.array(mean_user, dtype=np.uint8)
+            cv2.imwrite("../processed/"+exp +
+                        "/"+tp+"/"+user+"/" + "mean_" + user + ".jpg", mean_user)
+            cv2.imshow("Mean User", mean_user)
+            # cv2.waitKey(0)
+    mean_exp /= num
+    mean_exp *= -1
+    mean_exp += 255
+    mean_exp = np.array(mean_exp, dtype=np.uint8)
+    cv2.imwrite("../processed/"+exp + "/mean_" + exp + ".jpg", mean_exp)
+    cv2.imshow("Mean Exp", mean_exp)
+    # cv2.waitKey(0)
