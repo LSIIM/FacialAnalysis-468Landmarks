4fb21b9f613db1f35b75f2ec1db99bde35ec5199
CodeWracker <rodrigo.souza289@etec.sp.gov.br>
2021-12-10 00:05:19 -0300

    adicionei o script pra pegar o std_dev

---
+++ 02-pattern_analysis_exp/MaskStats_module.py
@@ -0,0 +1,57 @@
+import pandas as pd
+import numpy as np
+import os
+class MaskStats():
+    def __init__(self, emotion, tp):
+        self.emotion = "0"+str(emotion)
+        self.tp = "0"+str(tp)
+        self.df = pd.DataFrame()
+        self.qtd_masks = 0
+
+    def verify_mask_type(self, arq_name):
+        exp,tp = arq_name.split("_")[0].split("-")
+        return exp == self.emotion and tp == self.tp
+    def add_distances(self,dist_df):
+        new_dists = np.array(dist_df["distances"])
+        if(len(self.df)==0):
+            old_dists = np.zeros(new_dists.shape)
+        else:
+            old_dists = np.array(self.df["distances"])
+        self.df["distances"] = old_dists + new_dists
+        self.qtd_masks+=1
+        return    
+
+    def calculate_dist_means(self):
+        old_dists = np.array(self.df["distances"])
+        self.df["distances"] = old_dists/self.qtd_masks
+        return
+
+    def calculate_deviation_means(self):
+        old_deviations = np.array(self.df["std_deviation"])
+        self.df["std_deviation"] = old_deviations/self.qtd_masks
+        return
+    
+    def add_std_deviation(self,dist_df):
+        
+        new_dists = np.array(dist_df["distances"])
+        mean_dists = np.array(self.df["distances"])
+        deviation = np.sqrt((mean_dists - new_dists)**2)
+        try:
+            self.df["std_deviation"]
+        except:
+            self.df["std_deviation"] = np.zeros(deviation.shape)
+        
+        self.df["std_deviation"] = np.array(self.df["std_deviation"]) + deviation
+        self.qtd_masks+=1
+    def get_dataframe(self):
+        return self.df
+    def load_df(self):
+        self.df = pd.read_csv("./results/analysis/"+self.emotion+"-"+self.tp+"/distances-stats.csv")
+
+    
+    def save_dataframe(self):
+        try:
+            os.mkdir("./results/analysis/"+self.emotion+"-"+self.tp)
+        except:
+            None
+        self.df.to_csv("./results/analysis/"+self.emotion+"-"+self.tp+"/distances-stats.csv")
--- 02-pattern_analysis_exp/deviation_extractor.py
+++ 02-pattern_analysis_exp/deviation_extractor.py
@@ -17,15 +17,18 @@ parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
 sys.path.insert(0, parent_dir)
 from definitions import *
 
-
-
 if __name__ == "__main__":
     
     users = os.listdir(PATTERN_TEST_PATH)
     saves = []
     for i in range(8):
         for j in range(2):
-            saves.append(MaskStats(i,j))
+            mask_obj = MaskStats(i,j)
+            exp = "0"+str(i)
+            tp = "0"+str(j)
+            df = pd.read_csv("./results/analysis/"+exp+"/"+tp+"/distances-stats.csv")
+            mask_obj.load_df(df)
+            saves.append()
     
     for user in tqdm(users):
         arqs = os.listdir(PATTERN_TEST_PATH+"/"+user)
@@ -40,4 +43,27 @@ if __name__ == "__main__":
                     break
     for i in range(len(saves)):
         saves[i].calculate_dist_means()
+        saves[i].save_dataframe()
+
+
+if __name__ == "__main__":
+    
+    exps = os.listdir("./results/analysis")
+    saves = []
+    for i in range(8):
+        for j in range(2):
+            saves.append(MaskStats(i,j))
+    
+    for exp in tqdm(exps):
+        tps = os.listdir("./results/analysis/"+exp)
+        for tp in tps:
+            arq_path = "./results/analysis/"+exp + "/"+tp +"/distances-stats.csv"
+
+            df = pd.read_csv(arq_path)
+            for i in range(len(saves)):
+                if(saves[i].verify_mask_type(arq)):
+                    saves[i].add_std_deviation(df)
+                    break
+    for i in range(len(saves)):
+        saves[i].calculate_deviation_means()
         saves[i].save_dataframe()
