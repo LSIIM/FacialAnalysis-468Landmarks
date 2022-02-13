bced7a40e07145364997531a77b52e3efccbd034
CodeWracker <dev.rodrigofs@gmail.com>
2022-02-13 16:24:24 -0300

    arrumei os diretorios

---
+++ 02-pattern_analysis_exp/analysis-all-points/definitions.py
@@ -0,0 +1,12 @@
+initial_image_width = 1920
+p10_height = 25
+face_height = 1500
+face_margin = 25
+final_image_size_width = 2500
+final_image_size_height = 1900
+emotions_list = ["Neutral", "Angry", "Contempt",
+            "Disgusting", "Fear", "Happy", "Sad", "Surprise"]
+DATASET_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\migma_dataset"
+PROCESSED_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\processed"
+PATTERN_TEST_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\pattern_analysis"
+FORMS_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\questionarios"
--- 02-pattern_analysis_exp/analysis-all-points/main.py
+++ 02-pattern_analysis_exp/analysis-all-points/main.py
@@ -6,14 +6,7 @@ from multiprocessing import Process
 
 
 from distances_module import MaskDistances
-from inspect import getsourcefile
-import os.path
-import sys
-current_path = os.path.abspath(getsourcefile(lambda:0))
-current_dir = os.path.dirname(current_path)
-parent_dir = current_dir[:current_dir.rfind(os.path.sep)]
 
-sys.path.insert(0, parent_dir)
 from definitions import *
 
 
