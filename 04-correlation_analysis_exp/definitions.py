4cc8f2432d1a995425c6099f40b3ea3e53e5d3e3
CodeWracker <dev.rodrigofs@gmail.com>
2022-02-16 00:47:46 -0300

    arrumei os nomes

---
+++ 04-correlation_analysis_exp/definitions.py
@@ -0,0 +1,13 @@
+initial_image_width = 1920
+p10_height = 25
+face_height = 1500
+face_margin = 25
+final_image_size_width = 2500
+final_image_size_height = 1900
+emotions_list = ["Neutral", "Angry", "Contempt",
+            "Disgusting", "Fear", "Happy", "Sad", "Surprise"]
+DATASET_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\migma_dataset"
+PROCESSED_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\processed"
+PATTERN_TEST_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\pattern_analysis"
+PATTERN_TEST_PATH_SAME_POINT = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\pattern_analysis_same_point"
+FORMS_PATH = r"D:\OneDrive - UFSC\Academico\UFSC\MIGMA\questionarios"
